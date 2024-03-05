d7e1132637a4c7c54098b34fca30958e31c13235
Ohjintaek <jt950930@gmail.com>
2024-03-05 14:16:16 +0900

    13908.py : 완전 탐색

---
+++ Baekjoon/brute-force/13908.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+answer_numbers = set(map(str, input().split()))
+cnt = 0
+
+def recursive(N, number):
+    global cnt
+    if N == n:
+        if number
+        cnt += 1
+        return
+    for i in range(10):
