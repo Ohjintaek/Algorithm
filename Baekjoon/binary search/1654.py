a9c64781d1562affedb747e27a42fb1a9f44f4a6
Ohjintaek <jt950930@gmail.com>
2024-03-05 21:32:02 +0900

    1654.py : 이진 탐색

---
+++ Baekjoon/binary search/1654.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+lines = []
+for _ in range(K):
+    lines.append(int(input()))
+
+left = 1
+right = min(lines)
+
+while left < right:
+    mid = (left + right) // 2
+    cnt = 0
+    for line in lines:
+        cnt += line // mid
+    
+    if cnt >= N:
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(right)
