e01f7c51b295508c5544515a7522e544ae11b92e
Ohjintaek <jt950930@gmail.com>
2024-03-20 23:15:35 +0900

    백준 15565.py : 투 포인터

---
+++ Baekjoon/two pointer/15565.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+dolls = list(map(int, input().split()))
+
+cnt = 0
+ans = N + 1
+left = 0
+for right in range(N):
+    if dolls[right] == 1:
+        cnt += 1
+    
+    while True:
+        if dolls[left] == 2:
+            left += 1
+        elif cnt == K + 1 and dolls[left] == 1:
+            cnt -= 1
+            left += 1
+        else:
+            break
+    
+    if cnt == K:
+        ans = min(ans, right - left + 1)
+
+if ans == N + 1:
+    print(-1)
+else:
+    print(ans)
