9dc1a4e2f2ad9c0372722aabd4aba92b4ae224ae
Ohjintaek <jt950930@gmail.com>
2024-03-21 21:15:38 +0900

    백준 1904.py : 동적 계획법

--- Baekjoon/dynamic programming/15624.py
+++ Baekjoon/dynamic programming/15624.py
@@ -2,9 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+mod = 1000000007
 
 fibonacci = [0,1]
 for _ in range(n-1):
-    fibonacci.append((fibonacci[-2] + fibonacci[-1]) % 1000000007)
+    # 모듈로 연산
+    fibonacci.append((fibonacci[-2] % mod + fibonacci[-1] % mod) % mod)
 
-print(fibonacci[-1])
+if n == 0:
+    print(0)
+else:
+    print(fibonacci[-1])
