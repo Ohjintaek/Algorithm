c38ed65d5aec37ce7adee7305c6b25dd8570612a
Ohjintaek <jt950930@gmail.com>
2024-03-22 23:29:51 +0900

    백준 2263.py : 시간초과 해결

--- Baekjoon/tree/2263.py
+++ Baekjoon/tree/2263.py
@@ -1,10 +1,13 @@
 import sys
-sys.setrecursionlimit(10000000)
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 n = int(input())
 inOrder = list(map(int, input().split()))
 postOrder = list(map(int, input().split()))
+indexes = [0]*(n+1)
+for i in range(n):
+    indexes[inOrder[i]] = i
 
 def getPreOrder(postIndex, inIndex, length):
     if length == 0:
@@ -15,11 +18,7 @@ def getPreOrder(postIndex, inIndex, length):
     if length == 1:
         return
     
-    left = 0
-    for i in range(length):
-        if value == inOrder[inIndex + i]:
-            left = i
-            break
+    left = indexes[value] - inIndex
     
     getPreOrder(postIndex, inIndex, left)
     getPreOrder(postIndex + left, inIndex + left + 1, length - left - 1)
